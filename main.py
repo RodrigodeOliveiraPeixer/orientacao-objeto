7bd877dbbea38b537573baf81503ef400ec3b747
RodrigodeOliveiraPeixer <rodrigo.oliveira.peixer@gmail.com>
2022-10-11 20:44:43 -0300

    Praticando orientação objeto

---
+++ main.py
@@ -0,0 +1,10 @@
+from pessoa import Pessoa
+
+p1 = Pessoa('Luiz', 29)
+p1.comer('Maçã')
+p1.falar('POO')
+p1.parar_comer()
+p1.falar('POO')
+p1.comer('alimento')
+p1.parar_falar()
+p1.falar('assunto')
